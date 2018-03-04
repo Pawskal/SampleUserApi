e9b5022577306fabed1f71bc76d7e32027099421
pavel.ivanov <ipawskal@gmail.com>
2018-03-04 13:47:30 +0200

    initial dev

--- Models/User.cs
+++ Models/User.cs
@@ -7,10 +7,5 @@ namespace DotNetServer.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public int ID { get; set; }
-        // public UserModel(string name, string email){
-        //     Name = name;
-        //     Email = email;
-        //     ID = new Random().Next();
-        // }
     }
 }
